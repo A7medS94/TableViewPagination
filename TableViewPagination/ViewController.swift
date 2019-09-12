48121ab9e590c82d270ee51d145e412b72bf9ddb
A7medS94 <a.zaki@smartappco.com>
2019-09-12 14:03:01 +0200

    Initial Commit

---
+++ TableViewPagination/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewPagination
+//
+//  Created by iMac on 9/12/19.
+//  Copyright © 2019 Ahmed Samir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
